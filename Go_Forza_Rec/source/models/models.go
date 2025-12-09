d954a18a9a28f9f9e899e59bcb73dc3998305f55
Azrael <NONYA@Buissness.com>
2025-12-09 01:26:43 +0000

    updated to capture all telemetry headers available

--- Go_Forza_Rec/source/models/models.go
+++ Go_Forza_Rec/source/models/models.go
@@ -17,22 +17,94 @@ func (c *Car) AddState(s Carstate) {
 }
 
 type Carstate struct {
-	Timestamp     string
-	IsRaceOn      bool
-	TimestampMS   uint32
-	SpeedMPS      float64
-	SpeedKPH      float64
-	SpeedMPH      float64
-	Gear          int
-	EngineMaxRPM  float64
-	EngineIdleRPM float64
-	EngineRPM     float64
-	AccelX        float64
-	AccelY        float64
-	AccelZ        float64
-	VelX          float64
-	VelY          float64
-	VelZ          float64
+	Timestamp           string
+	IsRaceOn            bool
+	TimestampMS         uint32
+	EngineMaxRPM        float64
+	EngineIdleRPM       float64
+	EngineCurrentRPM    float64
+	AccelX              float64
+	AccelY              float64
+	AccelZ              float64
+	VelX                float64
+	VelY                float64
+	VelZ                float64
+	AngVelX             float64
+	AngVelY             float64
+	AngVelZ             float64
+	Yaw                 float64
+	Pitch               float64
+	Roll                float64
+	NormSuspFL          float64
+	NormSuspFR          float64
+	NormSuspRL          float64
+	NormSuspRR          float64
+	TireSlipFL          float64
+	TireSlipFR          float64
+	TireSlipRL          float64
+	TireSlipRR          float64
+	WheelRotFL          float64
+	WheelRotFR          float64
+	WheelRotRL          float64
+	WheelRotRR          float64
+	WheelOnRumbleFL     float64
+	WheelOnRumbleFR     float64
+	WheelOnRumbleRL     float64
+	WheelOnRumbleRR     float64
+	WheelInPuddleFL     float64
+	WheelInPuddleFR     float64
+	WheelInPuddleRL     float64
+	WheelInPuddleRR     float64
+	SurfaceRumbleFL     float64
+	SurfaceRumbleFR     float64
+	SurfaceRumbleRL     float64
+	SurfaceRumbleRR     float64
+	TireSlipAngleFL     float64
+	TireSlipAngleFR     float64
+	TireSlipAngleRL     float64
+	TireSlipAngleRR     float64
+	TireCombinedSlipFL  float64
+	TireCombinedSlipFR  float64
+	TireCombinedSlipRL  float64
+	TireCombinedSlipRR  float64
+	SuspTravelFL        float64
+	SuspTravelFR        float64
+	SuspTravelRL        float64
+	SuspTravelRR        float64
+	CarOrdinal          int
+	CarClass            int
+	CarPerformanceIndex int
+	DrivetrainType      int
+	NumCylinders        int
+	PosX                float64
+	PosY                float64
+	PosZ                float64
+	SpeedMPS            float64
+	SpeedKPH            float64
+	SpeedMPH            float64
+	Power               float64
+	Torque              float64
+	TireTempFL          float64
+	TireTempFR          float64
+	TireTempRL          float64
+	TireTempRR          float64
+	Boost               float64
+	Fuel                float64
+	Distance            float64
+	BestLap             float64
+	LastLap             float64
+	CurrentLap          float64
+	CurrentRaceTime     float64
+	LapNumber           int
+	RacePosition        int
+	Accel               int
+	Brake               int
+	Clutch              int
+	Handbrake           int
+	Gear                int
+	Steer               int
+	NormDrivingLine     int
+	NormAIBrakeDiff     int
 }
 
 // ExportCSV writes a car's telemetry to a CSV file named <CarName>.csv
@@ -53,19 +125,91 @@ func (c *Car) ExportCSV() error {
 		"timestamp",
 		"isRaceOn",
 		"timestampMS",
-		"speed_mps",
-		"speed_kph",
-		"speed_mph",
-		"gear",
-		"engine_rpm",
 		"engine_max_rpm",
 		"engine_idle_rpm",
+		"engine_current_rpm",
 		"accel_x",
 		"accel_y",
 		"accel_z",
 		"vel_x",
 		"vel_y",
 		"vel_z",
+		"ang_vel_x",
+		"ang_vel_y",
+		"ang_vel_z",
+		"yaw",
+		"pitch",
+		"roll",
+		"norm_susp_fl",
+		"norm_susp_fr",
+		"norm_susp_rl",
+		"norm_susp_rr",
+		"tire_slip_fl",
+		"tire_slip_fr",
+		"tire_slip_rl",
+		"tire_slip_rr",
+		"wheel_rot_fl",
+		"wheel_rot_fr",
+		"wheel_rot_rl",
+		"wheel_rot_rr",
+		"wheel_on_rumble_fl",
+		"wheel_on_rumble_fr",
+		"wheel_on_rumble_rl",
+		"wheel_on_rumble_rr",
+		"wheel_in_puddle_fl",
+		"wheel_in_puddle_fr",
+		"wheel_in_puddle_rl",
+		"wheel_in_puddle_rr",
+		"surface_rumble_fl",
+		"surface_rumble_fr",
+		"surface_rumble_rl",
+		"surface_rumble_rr",
+		"tire_slip_angle_fl",
+		"tire_slip_angle_fr",
+		"tire_slip_angle_rl",
+		"tire_slip_angle_rr",
+		"tire_combined_slip_fl",
+		"tire_combined_slip_fr",
+		"tire_combined_slip_rl",
+		"tire_combined_slip_rr",
+		"susp_travel_fl",
+		"susp_travel_fr",
+		"susp_travel_rl",
+		"susp_travel_rr",
+		"car_ordinal",
+		"car_class",
+		"car_performance_index",
+		"drivetrain_type",
+		"num_cylinders",
+		"pos_x",
+		"pos_y",
+		"pos_z",
+		"speed_mps",
+		"speed_kph",
+		"speed_mph",
+		"power",
+		"torque",
+		"tire_temp_fl",
+		"tire_temp_fr",
+		"tire_temp_rl",
+		"tire_temp_rr",
+		"boost",
+		"fuel",
+		"distance",
+		"best_lap",
+		"last_lap",
+		"current_lap",
+		"current_race_time",
+		"lap_number",
+		"race_position",
+		"accel",
+		"brake",
+		"clutch",
+		"handbrake",
+		"gear",
+		"steer",
+		"norm_driving_line",
+		"norm_ai_brake_diff",
 	}
 	if err := w.Write(header); err != nil {
 		return err
@@ -77,19 +221,91 @@ func (c *Car) ExportCSV() error {
 			s.Timestamp,
 			boolToStr(s.IsRaceOn),
 			u32(s.TimestampMS),
-			f64(s.SpeedMPS),
-			f64(s.SpeedKPH),
-			f64(s.SpeedMPH),
-			i(s.Gear),
-			f64(s.EngineRPM),
 			f64(s.EngineMaxRPM),
 			f64(s.EngineIdleRPM),
+			f64(s.EngineCurrentRPM),
 			f64(s.AccelX),
 			f64(s.AccelY),
 			f64(s.AccelZ),
 			f64(s.VelX),
 			f64(s.VelY),
 			f64(s.VelZ),
+			f64(s.AngVelX),
+			f64(s.AngVelY),
+			f64(s.AngVelZ),
+			f64(s.Yaw),
+			f64(s.Pitch),
+			f64(s.Roll),
+			f64(s.NormSuspFL),
+			f64(s.NormSuspFR),
+			f64(s.NormSuspRL),
+			f64(s.NormSuspRR),
+			f64(s.TireSlipFL),
+			f64(s.TireSlipFR),
+			f64(s.TireSlipRL),
+			f64(s.TireSlipRR),
+			f64(s.WheelRotFL),
+			f64(s.WheelRotFR),
+			f64(s.WheelRotRL),
+			f64(s.WheelRotRR),
+			f64(s.WheelOnRumbleFL),
+			f64(s.WheelOnRumbleFR),
+			f64(s.WheelOnRumbleRL),
+			f64(s.WheelOnRumbleRR),
+			f64(s.WheelInPuddleFL),
+			f64(s.WheelInPuddleFR),
+			f64(s.WheelInPuddleRL),
+			f64(s.WheelInPuddleRR),
+			f64(s.SurfaceRumbleFL),
+			f64(s.SurfaceRumbleFR),
+			f64(s.SurfaceRumbleRL),
+			f64(s.SurfaceRumbleRR),
+			f64(s.TireSlipAngleFL),
+			f64(s.TireSlipAngleFR),
+			f64(s.TireSlipAngleRL),
+			f64(s.TireSlipAngleRR),
+			f64(s.TireCombinedSlipFL),
+			f64(s.TireCombinedSlipFR),
+			f64(s.TireCombinedSlipRL),
+			f64(s.TireCombinedSlipRR),
+			f64(s.SuspTravelFL),
+			f64(s.SuspTravelFR),
+			f64(s.SuspTravelRL),
+			f64(s.SuspTravelRR),
+			i(s.CarOrdinal),
+			i(s.CarClass),
+			i(s.CarPerformanceIndex),
+			i(s.DrivetrainType),
+			i(s.NumCylinders),
+			f64(s.PosX),
+			f64(s.PosY),
+			f64(s.PosZ),
+			f64(s.SpeedMPS),
+			f64(s.SpeedKPH),
+			f64(s.SpeedMPH),
+			f64(s.Power),
+			f64(s.Torque),
+			f64(s.TireTempFL),
+			f64(s.TireTempFR),
+			f64(s.TireTempRL),
+			f64(s.TireTempRR),
+			f64(s.Boost),
+			f64(s.Fuel),
+			f64(s.Distance),
+			f64(s.BestLap),
+			f64(s.LastLap),
+			f64(s.CurrentLap),
+			f64(s.CurrentRaceTime),
+			i(s.LapNumber),
+			i(s.RacePosition),
+			i(s.Accel),
+			i(s.Brake),
+			i(s.Clutch),
+			i(s.Handbrake),
+			i(s.Gear),
+			i(s.Steer),
+			i(s.NormDrivingLine),
+			i(s.NormAIBrakeDiff),
 		}
 
 		if err := w.Write(record); err != nil {
--- Go_Forza_Rec/source/parser/parser.go
+++ Go_Forza_Rec/source/parser/parser.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"bytes"
 	"encoding/binary"
 	"fmt"
 	"forza/models"
@@ -9,56 +8,129 @@ import (
 	"time"
 )
 
-type recievedcarstate struct {
-	IsRaceOn         int8
-	TimestampMS      uint32
-	EngineMaxRPM     float32
-	EngineIdleRPM    float32
-	CurrentEngineRPM float32
-	AccelX           float32
-	AccelY           float32
-	AccelZ           float32
-	VelX             float32
-	VelY             float32
-	VelZ             float32
-}
-
 func RawtoCarstate(data []byte) (models.Carstate, error) {
-	if len(data) < 320 {
-		return models.Carstate{}, fmt.Errorf("packet too small")
+	const (
+		requiredBytes    = 323 // need up through byte 322 (indexing from 0)
+		cutStart         = 232 // bytes we keep before the gap
+		cutEnd           = 244 // start of the chunk to skip
+		patchedTotalSize = 311
+	)
+
+	if len(data) < requiredBytes {
+		return models.Carstate{}, fmt.Errorf("packet too small: %d bytes", len(data))
 	}
 
-	var pkt recievedcarstate
+	// Mirror the Python parser: drop bytes 232–243 (12 bytes) before unpacking.
+	patched := make([]byte, 0, patchedTotalSize)
+	patched = append(patched, data[:cutStart]...)
+	patched = append(patched, data[cutEnd:requiredBytes]...)
 
-	err := binary.Read(bytes.NewReader(data[:44]), binary.LittleEndian, &pkt)
-	if err != nil {
-		return models.Carstate{}, err
+	f32 := func(off int) float64 {
+		return float64(math.Float32frombits(binary.LittleEndian.Uint32(patched[off : off+4])))
 	}
+	i32 := func(off int) int {
+		return int(int32(binary.LittleEndian.Uint32(patched[off : off+4])))
+	}
+	u16 := func(off int) uint16 {
+		return binary.LittleEndian.Uint16(patched[off : off+2])
+	}
+	i8 := func(off int) int8 { return int8(patched[off]) }
+	u8 := func(off int) uint8 { return patched[off] }
 
-	gear := data[319]
-
-	speedMps := math.Sqrt(float64(
-		pkt.VelX*pkt.VelX +
-			pkt.VelY*pkt.VelY +
-			pkt.VelZ*pkt.VelZ,
-	))
+	speedFromVec := math.Sqrt(f32(32)*f32(32) + f32(36)*f32(36) + f32(40)*f32(40))
+	speedMps := f32(244)
+	if speedMps == 0 {
+		speedMps = speedFromVec
+	}
 
 	return models.Carstate{
-		Timestamp:     time.Now().Format(time.RFC3339Nano),
-		IsRaceOn:      pkt.IsRaceOn != 0,
-		TimestampMS:   pkt.TimestampMS,
-		SpeedMPS:      speedMps,
-		SpeedKPH:      speedMps * 3.6,
-		SpeedMPH:      speedMps * 2.23694,
-		Gear:          int(gear),
-		EngineMaxRPM:  float64(pkt.EngineMaxRPM),
-		EngineIdleRPM: float64(pkt.EngineIdleRPM),
-		EngineRPM:     float64(pkt.CurrentEngineRPM),
-		AccelX:        float64(pkt.AccelX),
-		AccelY:        float64(pkt.AccelY),
-		AccelZ:        float64(pkt.AccelZ),
-		VelX:          float64(pkt.VelX),
-		VelY:          float64(pkt.VelY),
-		VelZ:          float64(pkt.VelZ),
+		Timestamp:           time.Now().Format(time.RFC3339Nano),
+		IsRaceOn:            i32(0) != 0,
+		TimestampMS:         binary.LittleEndian.Uint32(patched[4:8]),
+		EngineMaxRPM:        f32(8),
+		EngineIdleRPM:       f32(12),
+		EngineCurrentRPM:    f32(16),
+		AccelX:              f32(20),
+		AccelY:              f32(24),
+		AccelZ:              f32(28),
+		VelX:                f32(32),
+		VelY:                f32(36),
+		VelZ:                f32(40),
+		AngVelX:             f32(44),
+		AngVelY:             f32(48),
+		AngVelZ:             f32(52),
+		Yaw:                 f32(56),
+		Pitch:               f32(60),
+		Roll:                f32(64),
+		NormSuspFL:          f32(68),
+		NormSuspFR:          f32(72),
+		NormSuspRL:          f32(76),
+		NormSuspRR:          f32(80),
+		TireSlipFL:          f32(84),
+		TireSlipFR:          f32(88),
+		TireSlipRL:          f32(92),
+		TireSlipRR:          f32(96),
+		WheelRotFL:          f32(100),
+		WheelRotFR:          f32(104),
+		WheelRotRL:          f32(108),
+		WheelRotRR:          f32(112),
+		WheelOnRumbleFL:     f32(116),
+		WheelOnRumbleFR:     f32(120),
+		WheelOnRumbleRL:     f32(124),
+		WheelOnRumbleRR:     f32(128),
+		WheelInPuddleFL:     f32(132),
+		WheelInPuddleFR:     f32(136),
+		WheelInPuddleRL:     f32(140),
+		WheelInPuddleRR:     f32(144),
+		SurfaceRumbleFL:     f32(148),
+		SurfaceRumbleFR:     f32(152),
+		SurfaceRumbleRL:     f32(156),
+		SurfaceRumbleRR:     f32(160),
+		TireSlipAngleFL:     f32(164),
+		TireSlipAngleFR:     f32(168),
+		TireSlipAngleRL:     f32(172),
+		TireSlipAngleRR:     f32(176),
+		TireCombinedSlipFL:  f32(180),
+		TireCombinedSlipFR:  f32(184),
+		TireCombinedSlipRL:  f32(188),
+		TireCombinedSlipRR:  f32(192),
+		SuspTravelFL:        f32(196),
+		SuspTravelFR:        f32(200),
+		SuspTravelRL:        f32(204),
+		SuspTravelRR:        f32(208),
+		CarOrdinal:          i32(212),
+		CarClass:            i32(216),
+		CarPerformanceIndex: i32(220),
+		DrivetrainType:      i32(224),
+		NumCylinders:        i32(228),
+		PosX:                f32(232),
+		PosY:                f32(236),
+		PosZ:                f32(240),
+		SpeedMPS:            speedMps,
+		SpeedKPH:            speedMps * 3.6,
+		SpeedMPH:            speedMps * 2.23694,
+		Power:               f32(248),
+		Torque:              f32(252),
+		TireTempFL:          f32(256),
+		TireTempFR:          f32(260),
+		TireTempRL:          f32(264),
+		TireTempRR:          f32(268),
+		Boost:               f32(272),
+		Fuel:                f32(276),
+		Distance:            f32(280),
+		BestLap:             f32(284),
+		LastLap:             f32(288),
+		CurrentLap:          f32(292),
+		CurrentRaceTime:     f32(296),
+		LapNumber:           int(u16(300)),
+		RacePosition:        int(u8(302)),
+		Accel:               int(u8(303)),
+		Brake:               int(u8(304)),
+		Clutch:              int(u8(305)),
+		Handbrake:           int(u8(306)),
+		Gear:                int(u8(307)),
+		Steer:               int(i8(308)),
+		NormDrivingLine:     int(i8(309)),
+		NormAIBrakeDiff:     int(i8(310)),
 	}, nil
 }
